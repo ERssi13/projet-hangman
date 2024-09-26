882fd72148605d4d0c0db61b4febbe7776b9a6b6
enzo rossi <enzo.rossi13@ynov.com>
2024-09-26 09:26:14 +0200

    main.go

--- exec/main.go
+++ exec/main.go
@@ -1,1 +1,24 @@
-package projethangman
+package main
+
+import (
+	"fmt"
+	"os"
+	"projethangman"
+)
+
+func main() {
+	if len(os.Args) < 2 {
+		fmt.Println("Usage: go run exec/main.go <fichier_de_mots>")
+		return
+	}
+	nomFichier := os.Args[1]
+	mots, err := projethangman.LireFichier(nomFichier)
+	if err != nil {
+		fmt.Println("Erreur lors de la lecture du fichier :", err)
+		return
+	}
+	mot := projethangman.MotAleatoire(mots)
+	fmt.Println("Un mot a été choisi au hasard.")
+	nbEssais := 8
+	projethangman.Jouer(mot, nbEssais)
+}
