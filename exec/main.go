4e5e1a91a465ee33c6c73adff07c0f736f55a1ed
enzo rossi <enzo.rossi13@ynov.com>
2024-09-29 22:07:32 +0200

    jeu.go

--- exec/main.go
+++ exec/main.go
@@ -7,18 +7,28 @@ import (
 )
 
 func main() {
-	if len(os.Args) < 2 {
-		fmt.Println("Usage: go run exec/main.go <fichier_de_mots>")
-		return
+	var choix int 
+	for {
+		projethangman.Menu()
+		fmt.Scan(&choix)
+		switch choix {
+		case 1:
+			if len(os.Args) < 2 {
+				fmt.Println("Usage: go run exec/main.go <fichier_de_mots>")
+				return
+			}
+			nomFichier := os.Args[1]
+			mots, err := projethangman.LireFichier(nomFichier)
+			if err != nil {
+				fmt.Println("Erreur lors de la lecture du fichier :", err)
+				return
+			}
+			mot := projethangman.MotAleatoire(mots)
+			fmt.Println("Un mot a été choisi au hasard.")
+			nbEssais := 8
+			projethangman.Jouer(mot, nbEssais)
+		case 0:
+			return
+		}
 	}
-	nomFichier := os.Args[1]
-	mots, err := projethangman.LireFichier(nomFichier)
-	if err != nil {
-		fmt.Println("Erreur lors de la lecture du fichier :", err)
-		return
-	}
-	mot := projethangman.MotAleatoire(mots)
-	fmt.Println("Un mot a été choisi au hasard.")
-	nbEssais := 8
-	projethangman.Jouer(mot, nbEssais)
 }
